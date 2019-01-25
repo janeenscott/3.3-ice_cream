ae16d39f7951e963ba084131c911d079d31a5113
Janeen Scott <janeen.e.scott@gmail.com>
2019-01-24 23:57:40 -0500

    database at admin is fillable and html from base and home is populating screen

--- ice_cream/models.py
+++ ice_cream/models.py
@@ -2,22 +2,22 @@ from django.db import models
 # from models import base_flavor
 
 
-
 class IceCream (models.Model):
     base_flavor = (
         ('chocolate', 'Chocolate'),
         ('vanilla', 'Vanilla'),
     )
-    available_flavors = (
-        ('cookies and creme', 'Cookies and Cream'),
-        ('mooose tracks', 'Moose Tracks'),
-        ('chocolate cherry', 'Chocolate Cherry'),
+    available = (
+        ('daily', 'Daily'),
+        ('weekly', 'Weekly'),
+        ('seasonal', 'Seasonal'),
     )
     flavor = models.CharField(max_length=100)
     base = models.CharField(max_length=100, choices=base_flavor, default='Vanilla')
-    available = models.CharField(max_length=100, choices=available_flavors, default='Cookies and Creme')
+    availability = models.CharField(max_length=100, choices=available, default='Cookies and Creme')
     featured = models.BooleanField()
     date_churned = models.DateField()
 
     def __str__(self):
         return self.flavor
+
--- ice_cream/views.py
+++ ice_cream/views.py
@@ -3,5 +3,12 @@ from django.views.generic import TemplateView
 from . import models
 
 class HomeView(TemplateView):
-    template_name = 'home.html'
+    template_name = 'ice_cream/home.html'
 
+    def get_context_data(self, **kwargs):
+        chocolate_ice_cream = models.IceCream.objects.filter(base='Chocolate')
+
+        context = {
+            'chocolate_base': chocolate_ice_cream
+        }
+        return context
