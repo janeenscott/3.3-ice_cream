0949de6598e0def6670912f988f6e4c92185be44
Janeen Scott <janeen.e.scott@gmail.com>
2019-01-25 00:27:21 -0500

    template is working and models and views seem to be interacting. Can only pull one featured and no other lists.

--- ice_cream/views.py
+++ ice_cream/views.py
@@ -1,14 +1,22 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 from . import models
+from .models import IceCream
 
 class HomeView(TemplateView):
     template_name = 'ice_cream/home.html'
 
     def get_context_data(self, **kwargs):
-        chocolate_ice_cream = models.IceCream.objects.filter(base='Chocolate')
+        featured_flavors = IceCream.objects.filter(featured=True)
+        daily_flavors = IceCream.objects.filter(availability='daily')
+        weekly_flavors = IceCream.objects.filter(availability='weekly')
+        seasonal_flavors = IceCream.objects.filter(availability='seasonal')
 
         context = {
-            'chocolate_base': chocolate_ice_cream
+            'featured': featured_flavors,
+            'daily': daily_flavors,
+            'weekly': weekly_flavors,
+            'seasonal': seasonal_flavors
         }
+
         return context
